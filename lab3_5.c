4d56ea75ac098621e411cbd8b9b472ea60244bcc
6320502479 <pattrapol.J@ku.th>
2021-02-15 19:06:17 +0700

    ด

--- lab3_5.c
+++ lab3_5.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 int main()
 {
-    int k,m=2,in;
+    int k,m=2;
     scanf("%d",&k);
-    in=k;
-    int i=0,mod[3];
-        while(k != 0)
+    int in=k;
+    int i=0,mod[3]={0,0,0},c=0;
+        while(in != 0)
     {
-        if(k!=0)
+        if(in!=0)
         {
-            if(k%m==0)
+            if(in%m==0)
             {
-            k=k/m;
+            in=in/m;
             mod[i]=m;
             i++;
             }
             else
             m++;
-            if(k<m)
-            break;
+            if(in<m)
+                break;
         }
     }
+
     if(mod[0]!=mod[1] && mod[1]!=mod[2] && mod[0]!=mod[2])
+     c=1;
+    if(mod[2]==0)
+     c=0;
+    if(c=1)
     {
-        printf("%d is a Lucky Number.",in);
+        printf("%d is a Lucky Number.",k);
     }
-    else
+   else if(c==0)
     {
-        printf("%d is not a Lucky Number.",in);
+      printf("%d is not a Lucky Number.",k);
     }
 
+
 }
+
+
+
