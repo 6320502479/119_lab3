0286f132431703e8c2b986b652ec97e82c9226dd
6320502479 <pattrapol.J@ku.th>
2021-02-15 17:02:54 +0700

    เ

--- lab3_1.c
+++ lab3_1.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int point[5][4],i,j;
+    int point[5][4],i,j,sum[5],win=0,p=0;
     for(i=0;i<5;i++)
     {
+        sum[i]=0;
         for(j=0;j<4;j++)
+        {
             scanf("%d",&point[i][j]);
+            sum[i]=sum[i]+point[i][j];
+        }
+     if(win<sum[i])
+     {
+         win=sum[i];
+         p=i+1;
+     }
+
     }
-    for(i=0;i<5;i++)
-    {
-        for(j=0;j<4;j++)
-            printf("%d ",point[i][j]);
-            printf("\n");
-    }
+    printf("%d %d",p,win);
 }
